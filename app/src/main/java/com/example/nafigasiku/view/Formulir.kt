c9cc41ef30b59a2b69fbd8a0bfe26cc34161a7a6
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-07 15:53:58 +0700

    Mengatur tata letak utama halaman dengan menambahkan Column yang menggunakan paddingValues dari Scaffold sebagai modifier.

--- app/src/main/java/com/example/nafigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/nafigasiku/view/Formulir.kt
@@ -44,3 +44,8 @@ fun FormIsian(
                     containerColor = colorResource(id = R.color.teal_700))
             )
         }
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
