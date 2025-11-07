d4ebe9fc4647e26fe8c6b68a3341c5ff9b3f0054
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-07 20:15:49 +0700

    Menambahkan struktur navigasi menggunakan Jetpack Navigation Compose untuk menghubungkan halaman FormIsian dan TampilData.

---
+++ app/src/main/java/com/example/nafigasiku/Navigasiku.kt
@@ -0,0 +1,53 @@
+package com.example.nafigasiku
+
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavHostController
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.padding
+import com.example.nafigasiku.view.TampilData
+import com.example.navigasiku.view.FormIsian
+
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ){
+            composable(route = Navigasi.Formulir.name){
+                FormIsian(
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {cancelAndBackToFormulir(navController)}
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(route = Navigasi.Formulir.name, inclusive = false)
+}
